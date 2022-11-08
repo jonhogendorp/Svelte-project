3b5673c2edd27b992ccbb9b9ebb055301ab5701d
codegorilla <codegorilla@codegorillas-MacBook-Air.local>
2022-11-08 11:39:33 +0100

    test functions work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+    let count = 0;
+
+    function incrementBtn (){
+        count += 1;
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2>
+    Number:{count}
+</h2>
+<button on:click={incrementBtn}>click to increase</button>
+
+<a href="/about">go to about</a>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script></script>
+
+<h1>about what?</h1>
+
+<style>
+    
+</style>
